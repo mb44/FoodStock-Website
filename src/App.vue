2d9c24c71abcf44c5f0593282db59879e333bfee
Morten Beuchert <mortenbgood@gmail.com>
2018-03-06 16:58:08 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <fm-header></fm-header>
+      </div>
+      <div class="row">
+        <fm-home></fm-home>
+      </div>
+      <div class="row">
+      <fm-footer></fm-footer>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Home from './components/Home.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  data() {
+    
+  },
+  components: {
+    fmHeader: Header,
+    fmFooter: Footer,
+    fmHome: Home
+  }
+}
+</script>
+
+<style>
+
+</style>
