cfa8eb44ce8cd3d7b80072b392969cd7a5585b98
Morten Beuchert <mortenbgood@gmail.com>
2018-03-07 12:16:00 +0100

    Initial contents

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>This is the home page</h1>
+  <div>
+    <h2>Welcome to Food Stock Monitor</h2>
+    <p><strong>What would you like to do?</strong></p>
+
+    <nav class="nav flex-column">
+      <a class="nav-link active" href="#">Generate shopping list</a>
+      <a class="nav-link" href="#">Manage Containers</a>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -7,3 +15,10 @@ export default {
   
 }
 </script>
+
+<style scoped>
+h2 {
+  margin-top: 50px;
+}
+</style>
+
