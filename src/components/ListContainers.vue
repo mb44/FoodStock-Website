4302440ef349e118caa6106db4701def74d6001a
Morten Beuchert <mortenbgood@gmail.com>
2018-03-07 14:01:49 +0100

    Add table

--- src/components/ListContainers.vue
+++ src/components/ListContainers.vue
@@ -1,19 +1,47 @@
 <template>
-  <div>
+  <div class="main">
       <h3>Container List</h3>
 
-      
+      <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Food name</th>
+      <th scope="col">Current amount</th>
+    </tr>
+  </thead>
+  <tbody v-for="container in containers">
+    <tr>
+      <th scope="row">{{ container.id }}</th>
+      <td>{{ container.foodName }}</td>
+      <td><div>{{ container.currentAmount }}kg</div> <progress max="100" value="35"></progress></td>
+    </tr>
+  </tbody>
+</table>
+<button type="button" class="btn btn-primary">Primary</button>
 
   </div>
 </template>
 
 <script>
 export default {
-  
+  data: function() {
+      return {
+          containers: [ 
+              { id: 1, foodName: 'Coffee', currentAmount: 0.4, maxCapacity: 2.0, progress: Math.round(this.currentAmount*100/this.maxCapacity) },
+              { id: 2, foodName: 'Cashew Nuts', currentAmount: 0.1, maxCapacity: 0.5, progress: Math.round(this.currentAmount*100/this.maxCapacity) },
+              { id: 3, foodName: 'Sugar', currentAmount: 2.0, maxCapacity: 4.0, progress: Math.round(this.currentAmount*100/this.maxCapacity) },
+              { id: 4, foodName: 'Coffee', currentAmount: 9, progress: Math.round(this.currentAmount*100/this.maxCapacity) },
+              { id: 5, foodName: 'Eggs', currentAmount: 0.25, progress: Math.round(this.currentAmount*100/this.maxCapacity) },
+          ]
+      }
+  }     
 }
 </script>
 
-<style>
-
+<style scoped>
+.main {
+    margin-top: 50px;
+}
 </style>
 
