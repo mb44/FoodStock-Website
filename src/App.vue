27df842ee6f550f6a712016d9c2703ec85d14058
Morten Beuchert <mortenbgood@gmail.com>
2018-03-07 12:15:32 +0100

    Add dynamic component

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,45 @@
   <div id="app">
     <div class="container">
       <div class="row">
-        <fm-header></fm-header>
+        <fm-header @viewWasChanged="currentView = $event"></fm-header>
       </div>
       <div class="row">
-        <fm-home></fm-home>
+        <keep-alive>
+          <component v-bind:is="currentView"></component>
+        </keep-alive>        
       </div>
+      <!--
       <div class="row">
         <fm-footer></fm-footer>
       </div>
+      -->
     </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Home from './components/Home.vue'
 import Footer from './components/Footer.vue'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+import ListContainers from './components/ListContainers.vue'
+import ListFoodTypes from './components/ListFoodTypes.vue'
+import ListUsers from './components/ListUsers.vue'
 
 export default {
   data() {
-    
+    return {
+      currentView: 'fmLogin'
+    }
   },
   components: {
     fmHeader: Header,
     fmFooter: Footer,
-    fmHome: Home
+    fmHome: Home,
+    fmLogin: Login,
+    fmListContainers: ListContainers,
+    fmListFoodTypes: ListFoodTypes,
+    fmListUsers: ListUsers
   }
 }
 </script>
