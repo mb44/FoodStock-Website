4bfbe09d573b4ca8b992481196bc1e0e8c0720b9
Morten Beuchert <mortenbgood@gmail.com>
2018-03-07 12:16:27 +0100

    Add event emitters from header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,17 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
-      <a class="navbar-brand" href="#">Food Stock Monitor</a>
+      <a class="navbar-brand">Food Stock Monitor</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Container Management</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Food Type Management</a>
-          </li>
+        <ul class="nav navbar-nav mr-auto">
+          <li class="nav-item"><a class="nav-link" href="#" @click="viewSelected='fmHome'" >Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="#" @click="viewSelected='fmListContainers'">Container Management</a></li>
+          <li class="nav-item"><a class="nav-link" href="#" @click="viewSelected='fmListFoodTypes'">Food Type Management</a></li>
+          <li class="nav-item"><a class="nav-link" href="#" @click="viewSelected='fmListUsers'">User Management</a></li>
         </ul>
       </div>
     </nav>
@@ -25,8 +20,17 @@
 
 <script>
 export default {
-  
-}
+        data: function() {
+            return {
+                viewSelected: ''
+            }
+        },
+        watch: {
+            viewSelected: function() {
+                this.$emit('viewWasChanged', this.viewSelected)
+            }
+        }
+    }
 </script>
 
 
