503355012dc327b3c5ba037a9c4c62755509d317
Morten Beuchert <mortenbeuchert@gmail.com>
2018-03-08 23:40:42 +0100

    Add components

---
+++ src/components/AddFoodType.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+      <h3>Add Food Type</h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.main {
+  margin-top: 50px;
+}
+</style>
+
+
---
+++ src/components/DeleteUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+      <h3>Delete User</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.main {
+  margin-top: 50px;
+}
+</style>
+
+
