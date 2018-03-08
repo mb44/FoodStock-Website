1a07c8260ef86c08803335a4128709925d8a7b02
Morten Beuchert <mortenbeuchert@gmail.com>
2018-03-09 00:09:36 +0100

    Add link functionality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,23 @@
     <h2>Welcome to Food Stock Monitor</h2>
     <p><strong>What would you like to do?</strong></p>
 
-    <nav class="nav flex-column">
-      <a class="nav-link active" href="#">Generate shopping list</a>
-      <a class="nav-link" href="#">Manage Containers</a>
-    </nav>
+    <ul>
+      <router-link to="/GenerateShoppingList" tag="a">Generate shopping list</router-link><br>
+      <router-link to="/ListContainers" tag="a">Manage Containers</router-link>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  
+  methods: {
+    gotoShoppingList() {
+      this.$router.push("/generateShoppingList")
+    },
+    gotoManageContainers() {
+      this.$router.push("/ListContainers")
+    }
+  }
 }
 </script>
 
