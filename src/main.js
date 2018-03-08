2d331c6faaa0c18333ef11ec9dae430622436e02
Morten Beuchert <mortenbeuchert@gmail.com>
2018-03-08 21:19:00 +0100

    Add Vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+// Make Vue router globally accessible without the need to import it
+Vue.use(VueRouter)
+
+// Setup Vue router instance
+const router = new VouRouter({
+
+})
 
 new Vue({
   el: '#app',
+  router, // Inject the router into this Vue instance
   render: h => h(App)
 })
