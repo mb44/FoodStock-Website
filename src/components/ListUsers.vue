9e6e80b9725dce0fa61fc0ff058544bb7bfa36a8
Morten Beuchert <mortenbgood@gmail.com>
2018-03-09 13:28:54 +0100

    Add table with data

--- src/components/ListUsers.vue
+++ src/components/ListUsers.vue
@@ -2,7 +2,42 @@
   <div class="main">
       <h3>User List</h3>
 
-      
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+            <th scope="col">Privileges</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Morten</th>
+            <td>mortenbeuchert@gmail.com</td>
+            <th scope="col">Admin</th>            
+          </tr>
+
+          <tr>
+            <th scope="row">Lars</th>
+            <td>lars@gmail.com</td>
+            <th scope="col">Admin</th>            
+          </tr>
+
+          <tr>
+            <th scope="row">NormalUser1</th>
+            <td>user1@yahoo.com</td>
+            <th scope="col">Staff</th>
+          </tr>
+
+          <tr>
+            <th scope="row">NormalUser1</th>
+            <td>user2@gmail.com</td>
+            <th scope="col">Staff</th>
+          </tr>
+        </tbody>
+      </table>
+
+      <router-link to="/AddUser" tag="button" class="btn btn-primary">Add User</router-link>
 
   </div>
 </template>
