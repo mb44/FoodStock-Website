0944258aa803d883804cce16a61059fb9b4dacd7
Morten Beuchert <mortenbgood@gmail.com>
2018-03-09 13:28:22 +0100

    Added routes

--- src/main.js
+++ src/main.js
@@ -2,20 +2,26 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import Login from './components/Login'
 import Home from './components/Home.vue'
 import ListContainers from './components/ListContainers.vue'
 import ListFoodTypes from './components/ListFoodTypes.vue'
 import ListUsers from './components/ListUsers.vue'
+import AddUser from './components/AddUser.vue'
+import AddFoodType from './components/AddFoodType.vue'
 import GenerateShoppingList from './components/GenerateShoppingList'
 
 // Make Vue router globally accessible without the need to import it
 Vue.use(VueRouter)
 
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: Home },,
+  { path: "/Login", component: Login },
   { path: "/ListContainers", component: ListContainers },
   { path: "/ListFoodTypes", component: ListFoodTypes },
+  { path: "/AddFoodType", component: AddFoodType },
   { path: "/ListUsers", component: ListUsers },
+  { path: "/AddUser", component: AddUser },
   { path: "/GenerateShoppingList", component: GenerateShoppingList },
   { path: "*", redirect: "/" }
 ]
