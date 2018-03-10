ed7822fd8f55c39f38d239036360830b072fd017
Morten Beuchert <mortenbeuchert@gmail.com>
2018-03-10 14:59:57 +0100

    New component

---
+++ src/components/AuthStatus.vue
@@ -0,0 +1,37 @@
+<template>
+<p><em>Signed in as: {{ getCurrentUser }}&nbsp;-&nbsp;<a @click.prevent="signOut">Sign out</a></em></p>
+</template>
+
+<script>
+import Firebase from 'firebase'
+    
+export default {
+    data: function() {
+        return {
+
+        }
+    },
+    computed: {
+        getCurrentUser() {
+            return this.$store.getters.getCurrentUser
+        }
+    },
+    methods: {
+        signOut() {
+            Firebase.auth().signOut().then(function() {
+                alert("Logged out")
+            }).catch(function(error) {
+                alert("Error")
+            })
+        }
+    } 
+}
+
+</script>
+
+<style>
+    a:hover {
+        cursor: pointer;
+    }
+</style>
+
