23d3f9fe8dd470ad4be21894c33266d429b57ee6
Morten Beuchert <mortenbeuchert@gmail.com>
2018-03-10 02:23:54 +0100

    Add Firebase support for foods and users

--- src/store/store.js
+++ src/store/store.js
@@ -8,18 +8,28 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
-        containerItems: []
+        containerItems: [],
+        foodItems: [],
+        userItems: []
     },
     mutations: {
-        // your mutations
+        // mutations (used my firebase to mutate states)
         ...firebaseMutations
     },
     actions: {
         setContainersRef: firebaseAction(({bindFirebaseRef}, ref) => {
           bindFirebaseRef('containerItems', ref)
         }),
+        setFoodsRef: firebaseAction(({bindFirebaseRef}, ref) => {
+            bindFirebaseRef('foodItems', ref)
+        }),
+        setUsersRef: firebaseAction(({bindFirebaseRef}, ref) => {
+            bindFirebaseRef('userItems', ref)
+        })
     },
     getters: {
-        getContainers: state => state.containerItems
+        getContainers: state => state.containerItems,
+        getFoods: state => state.foodItems,
+        getUsers: state => state.userItems
     }
 })
