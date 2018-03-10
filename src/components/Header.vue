99849fb52d337135b38d80114231f73825c8401f
Morten Beuchert <mortenbeuchert@gmail.com>
2018-03-10 15:00:22 +0100

    Add AuthStatus component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,33 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
-      <a class="navbar-brand">Food Stock Monitor</a>
+      <router-link to="/" tag="a"><a class="navbar-brand">Food Stock Monitor</a></router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="nav navbar-nav mr-auto">
-          <router-link to="/" tag="li"><a class="nav-link">Home</a></router-link>
           <router-link to="/listcontainers" tag="li"><a class="nav-link">Container Management</a></router-link>
           <router-link to="/listfoodtypes" tag="li"><a class="nav-link">Food Type Management</a></router-link>
-          <router-link to="/listusers" tag="li"><a class="nav-link">User Management</a></router-link>
+          <router-link to="/listusers" tag="li"><a class="nav-link">User Management</a></router-link>     
         </ul>
+        <div>
+          <fm-auth-status></fm-auth-status>
+        </div>
       </div>
     </nav>
+
+    
   </div>
 </template>
 
 <script>
+
+
 export default {
-        data: function() {
-            return {
-                viewSelected: ''
-            }
-        },
-        watch: {
-            viewSelected: function() {
-                this.$emit('viewWasChanged', this.viewSelected)
-            }
-        }
-    }
+
+}
 </script>
 
 
