bfd6483a8386c33de2b9b06daf7dcc421376bbba
Morten Beuchert <mortenbeuchert@gmail.com>
2018-04-07 18:45:55 +0200

    Remove button margin

--- src/components/AddFoodType.vue
+++ src/components/AddFoodType.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="main col-sm-12 col-md-6 p-0">
-      <h3>Add Food Type</h3>
-      <form class="form from-group-lg">
-        <div class="form-row">
-          <div class="col-md-6 mb-2">
-            <label for="inputFoodname">Food name:</label>
-          </div>
+    <h3>Add Food Type</h3>
+    <form class="form from-group-lg">
+      <div class="form-row">
+        <div class="col-md-6 mb-2">
+          <label for="inputFoodname">Food name:</label>
         </div>
-        <div class="form-row">
-          <div class="col-md-6 mb-2">
-            <label for="inputFoodname" class="sr-only">Food name:</label>
-            <input v-model="foodname" type="text" class="form-control" id="inputFoodname" placeholder="Enter food name">
-          </div>
+      </div>
+      <div class="form-row">
+        <div class="col-md-6 mb-2">
+          <label for="inputFoodname" class="sr-only">Food name:</label>
+          <input v-model="foodname" type="text" class="form-control" id="inputFoodname" placeholder="Enter food name">
         </div>
+      </div>
 
-        <div class="form-row">
-          <div class="col-md-6 mb-2">
-            <label for="inputReorderThreshold">Reorder threshold:</label>
-          </div>
+      <div class="form-row">
+        <div class="col-md-6 mb-2">
+          <label for="inputReorderThreshold">Reorder threshold:</label>
         </div>
-        <div class="form-row">
-          <div class="col-md-6 mb-2">
-            <label for="inputReorderThreshold" class="sr-only">Reorder threshold:</label>
-            <input v-model.number="reorderThreshold" type="text" class="form-control" id="inputReorderThreshoold" placeholder="Enter reorder threshold">
-          </div>
+      </div>
+      <div class="form-row">
+        <div class="col-md-6 mb-2">
+          <label for="inputReorderThreshold" class="sr-only">Reorder threshold:</label>
+          <input v-model.number="reorderThreshold" type="text" class="form-control" id="inputReorderThreshoold" placeholder="Enter reorder threshold">
         </div>
+      </div>
 
-        <div class="form-row">
-          <button type="submit" class="btn btn-primary mb-2" @click.prevent="addFoodType">Submit</button>
-        </div>
-      </form>
+      <div class="form-row">
+        <button type="submit" class="btn btn-primary" @click.prevent="addFoodType">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
 
